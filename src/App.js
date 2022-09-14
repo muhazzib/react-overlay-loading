bdee9951b99e8390cf89264270bb61fe8919330a
muhazzib <muhazzib96@gmail.com>
2022-09-14 17:41:35 +0500

    added demo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import OverlayLoader from "./components/Loader";
 
 function App() {
   return (
-    <OverlayLoader loadingText='Loading...' overlayContentStyle={{ width: '80px' }} />
+    <OverlayLoader loadingText='Loading...' />
   );
 }
 
