c570de6dea675710f37afb6d764289b3e5265719
muhazzib <muhazzib96@gmail.com>
2022-09-15 16:37:05 +0500

    added demo

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,31 +1,23 @@
 import './Loader.css';
 
 const OverlayLoader = (props) => {
-
-
     const { active = true, variant = "spinner", onClick, overlayContainerStyle = {}, contentContainerStyle = {}, loadingTextStyle = {}, loadingText, children } = props;
     const loaders = {
         'spinner': <div className={variant} />,
         'wheel': <div className={variant} />,
-        'facebook': <div className="facebook"><div /><div /><div /></div>,
-        'circle': <div className="circle"><div/></div>,
-        'ripple': <div className="ripple"><div/><div/></div>,
-        'ring': <div className="ring"></div>
+        'facebook': <div className={variant}><div /><div /><div /></div>,
+        'circle': <div className={variant}><div /></div>,
+        'ripple': <div className={variant}><div /><div /></div>,
+        'ring': <div className={variant} />
     };
 
-    const libraryLoaders = () => loaders[variant] ? loaders[variant] : loaders['wheel'];
+    const libraryLoaders = () => loaders[variant] || loaders['wheel'];
 
     return active ? (
         <div className="overlay_container" style={overlayContainerStyle} onClick={onClick}>
             <div className="overlay_content" style={contentContainerStyle}>
-                {
-                    children || libraryLoaders()
-                }
-                {
-                    loadingText && (
-                        <p style={{ loadingTextStyle }}>{loadingText}</p>
-                    )
-                }
+                { children || libraryLoaders() }
+                { loadingText && <p style={{ loadingTextStyle }}>{loadingText}</p> }
             </div>
         </div>
     ) : null;
